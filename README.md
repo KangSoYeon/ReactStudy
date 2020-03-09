dae52a9beeb0c4b466f055661816aa1f0140a800
KangSoYeon <aaa0311@naver.com>
2020-03-09 18:18:57 +0900

    첫날실습

--- README.md
+++ README.md
@@ -5,3 +5,28 @@
 ```
 yarn start
 ```
+
+React는 사용자 인터페이스를 구축하기 위한 선언적이고 효율적이며 유연한 JavaScript 라이브러리.
+"컴포넌트"라고 불리는 작고 고립된 코드의 파편을 이용하여 복잡한 UI를 구성하도록 돕는다.
+
+React Component의 하위 클래스
+
+```
+class ShoppingList extends React.Component {
+  render() {
+    return (
+      <div className="shopping-list">
+        <h1>Shopping List for {this.props.name}</h1>
+        <ul>
+          <li>Instagram</li>
+          <li>WhatsApp</li>
+          <li>Oculus</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+```
+* ShoppingList는 React컴포넌트 클래스 또는 React 컴포넌트 타입.
+* 개별 컴포넌트는 props라는 매개변수를 받아오고 render 함수를 통해 표시할 뷰 계층 구조를 반환
--- first/src/App.js
+++ first/src/App.js
@@ -1,12 +1,52 @@
 import React, { Component } from 'react';
 import TodoListTemplate from './components/TodoListTemplate';
 import Form from './components/Form';
+import TodoItemList from './components/TodoItemList';
 
 class App extends Component {
+
+  id = 3
+
+  state = {
+    input: '',
+    todos: [
+      { id: 0, text: '리액트 소개', checked: false },
+      { id: 1, text: '리액트 소개', checked: true },
+      { id: 2, text: '리액트 소개', checked: false }
+    ]
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      input: e.target.value
+    });
+  }
+
+  handleKeyPress = (e) => {
+    if (e.key === 'Enter') { //눌러진 키가 enter면 handleCreate호출
+      this.handleCreate();
+    }
+  }
   render() {
+    const { input } = this.state;
+    
+    //this를 앞에 계속 붙여줘야하는 작업 생략 가능
+    const {
+      handleChange,
+      handleCreate,
+      handleKeyPress
+    } = this;
+
     return (
-      <TodoListTemplate form={<Form/>}>
-        템플릿 완성
+      <TodoListTemplate form={(
+        <Form 
+          value={input}
+          onKeyPress={handleKeyPress}
+          onChange={handleChange}
+          onCreate={handleCreate}
+        />  
+      )}>
+        <TodoItemList />
       </TodoListTemplate>
     );
   }
