dcd179ef629c475d114433fbd2c62434e3feee6f
KangSoYeon <aaa0311@naver.com>
2020-03-10 17:51:53 +0900

    기능은 끄틴디ㅔ 에러가 있네

--- first/src/App.js
+++ first/src/App.js
@@ -18,7 +18,22 @@ class App extends Component {
 
   handleChange = (e) => {
     this.setState({
-      input: e.target.value
+      input: e.target.value //input의 다음 바뀔 값
+    });
+  }
+
+  //react에서는 배열에 push 사용하면 안됨.
+  //-> 배열에 값이 추가되긴 하지만 가르키고 있는 배열이 똑같아서 비교를 할 수 없음
+  //concat은 새로운 배열을 만들어서 괜찮음.
+  handleCreate = () => {
+    const {input, todos} = this.state;
+    this.setState({
+      input: '',
+      todos: todos.concat({ //concat을 사용하여 배열에 추가
+        id: this.id++,
+        text: input,
+        checked: false
+      })
     });
   }
 
@@ -27,8 +42,40 @@ class App extends Component {
       this.handleCreate();
     }
   }
+
+  handleToggle = (id) => {
+    const { todos } = this.state;
+
+    //파라미터로 받은 id를 가지고 몇번째 아이템인지 찾는다.
+    const index = todos.findIndex(todo => todo.id === id);
+    const selected = todos[index]; //선택한 객체
+
+    const nextTodos = [...todos]; //배열을 복사
+
+    //기존의 값들을 복사하고, checked 값을 덮어쓰기
+    //배열의 값을 직접 수정하면 절대 안됨!! 전개연산자를 이용하여 업데이트 해야함
+    nextTodos[index] = {
+      ...selected,
+      checked: !selected.checked
+    };
+
+    this.setState({
+      todos: nextTodos
+    });
+  }
+
+  handleRemove = (id) => {
+    const {todos} = this.state;
+    this.setState({
+      //파라미터로 받아온 id를 가지고 있지 않은 배열로 새로 생성한 것
+      //각 todo의 요소에 대해 todo.id !== id 의 true, false를 반환
+      //filter로 true값이 반환 된 값만 배열로 다시 생성
+      todos: todos.filter(todo => todo.id !== id) 
+    });
+  }
+
   render() {
-    const { input } = this.state;
+    const { input, todos } = this.state;
     
     //this를 앞에 계속 붙여줘야하는 작업 생략 가능
     const {
@@ -46,7 +93,7 @@ class App extends Component {
           onCreate={handleCreate}
         />  
       )}>
-        <TodoItemList />
+        <TodoItemList todos={todos} onToggle = {handleToggle}/>
       </TodoListTemplate>
     );
   }
