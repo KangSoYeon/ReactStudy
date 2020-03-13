56494dc4ef55d2e9c3eaf9ffcfec208d73316abf
KangSoYeon <aaa0311@naver.com>
2020-03-13 17:20:52 +0900

    금요일

--- second/src/App.js
+++ second/src/App.js
@@ -1,26 +1,37 @@
 import React, { Component } from 'react';
 import TOC from "./components/Toc";
+import Subject from "./components/Subject"
 import './App.css';
 
-class Subject extends Component{
+//부모인 App의 입장에서는 state라는 내부정보 사용
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      subject:{title:"WEB", sub: "World Wide Web"},
+      contents:[
+        {id:1, title:'HTML', desc: 'Html is for information'},
+        {id:2, title:'css', desc: 'CSS is for design'},
+        {id:3, title:'JavaScript', desc: 'Javascript is for interactive'},
+      ]
+    }
+  }
+
+  //그것을 자식 입장에서는 props의 역할로 전달
+  //app의 입장에서는 토픽이 내부적으로 어떻게 돌아가는지 알 필요 없음
+  //데이터라고 하는 props로는 어떤 형태의 정보를 전달하면 되는 가 
+  //--> 사용자의 입장에서 알아야 할것만 알면 됨
   render(){
-    return(
-      <header>
-        <h1>{this.props.title}</h1>
-        {this.props.sub}
-      </header>
+    return (
+      <div className="App">
+        <Subject title="WEB" sub="world wide web!"></Subject>
+        <Subject
+          title = {this.state.subject.title}
+          sub = {this.state.subject.sub}></Subject>
+        <TOC data={this.state.contents}></TOC>
+      </div>
     );
   }
-}
-
-function App() {
-  return (
-    <div className="App">
-      <Subject title="WEB" sub="world wide web!"></Subject>
-      <Subject title="React" sub="F"></Subject>
-      <TOC></TOC>
-    </div>
-  );
 }
 
 export default App;
--- second/src/components/Toc.js
+++ second/src/components/Toc.js
@@ -1,14 +1,19 @@
 import React, {Component} from 'react';
 
-
 class TOC extends Component{
     render(){
+        var lists = [];
+        //부모가 전달한 state를 자식은 props의 형태로 사용
+        var data = this.props.data; 
+        var i=0;
+        while(i<data.length){
+            lists.push(<li key={data[i].id}><a href={"/content/" + data[i].id}>{data[i].title}</a></li>)
+            i=i+1;
+        }
         return(
             <nav>
                 <ul>
-                    <li><a href="1.html">HTML</a></li>
-                    <li><a href="2.html">CSS</a></li>
-                    <li><a href="3.html">JavaScript</a></li>
+                   {lists}
                 </ul>
             </nav>
         );
